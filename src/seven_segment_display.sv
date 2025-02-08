8a34d953f05efba20933782862757d1466db1de7
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2025-02-08 22:13:18 +0300

    Update seven_segment_display.sv

--- src/seven_segment_display.sv
+++ src/seven_segment_display.sv
@@ -2,7 +2,7 @@ module seven_segment_display
 # (
     parameter w_digit   = 2,
     parameter clk_mhz   = 50,
-    parameter update_hz = 120  // The default of 4 is too low for FIFO lab // Looks like a sane default
+    parameter update_hz = 120
 )
 (
     input  clk,
